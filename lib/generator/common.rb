2b39718983cbadfacd9d4d042a843abe28ec7f70
Roman Efimov <romaonthego@gmail.com>
2012-07-26 21:17:34 -0500

    Human output when no params

--- lib/generator/common.rb
+++ lib/generator/common.rb
@@ -1,7 +1,7 @@
 module Generator
   @@config = {}
   @@properties = {}
-    
+
   def self.configure(options)
     @@config = options
     if options[:prefix].nil?
@@ -12,41 +12,41 @@ module Generator
       end
     end
   end
-  
+
   def self.config
     @@config
   end
-  
+
   def self.properties
     @@properties
   end
-  
+
   def self.validate!
     type = ARGV[0]
     if type != "model"
-      raise ArgumentError, "Unknown type '#{type}'.", caller
+      raise ArgumentError, "Unknown type '#{type}'.\nRun restkit-generate --help for more information.", caller
     end
-    
+
     @@config[:model_name] = ARGV[1]
     if @@config[:model_name].nil?
       raise ArgumentError, "Model name can't be empty.", caller
     end
-    
+
     ARGV.shift(2)
     if ARGV.nil?
       raise ArgumentError, "No input parameters defined.", caller
     end
-    
+
     ARGV.each do |arg|
       original_name = arg.split(":")[0]
       a = arg.gsub(/_([a-z])/) do |word|
         word.gsub(/_/, "").capitalize
       end
-      
+
       name = a.split(":")[0]
       name = @@config[:model_name].downcase+"Id" if name == "id"
       type = a.split(":")[1]
-      
+
       @@properties[name] = {:type => type, :original_name => original_name}
     end
   end
