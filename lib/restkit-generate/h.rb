391c5b0daed47eb7668c50896e0e919eee73cac6
Roman Efimov <romaonthego@gmail.com>
2013-02-11 14:16:50 -0600

    Change order for qualifier and atomic/nonatomic

--- lib/restkit-generate/h.rb
+++ lib/restkit-generate/h.rb
@@ -5,7 +5,7 @@ module RestKitGenerate
       model_name = RestKitGenerate.config[:model_name]
       prefix = RestKitGenerate.config[:prefix]
       core_data = RestKitGenerate.config[:core_data]
-      RestKitGenerate.properties.each{|key, value| props << "@property (nonatomic, #{value[:type] == "NSString" ? "copy" : "strong"}) #{value[:type]} *#{key};" }
+      RestKitGenerate.properties.each{|key, value| props << "@property (#{value[:type] == "NSString" ? "copy" : "strong"}, nonatomic) #{value[:type]} *#{key};" }
 
       erb = ERB.new(File.read(File.join($lib, 'restkit-generate', !core_data ? 'nsobject.h.erb' : 'nsmanagedobject.h.erb')))
       result = erb.result(binding)
